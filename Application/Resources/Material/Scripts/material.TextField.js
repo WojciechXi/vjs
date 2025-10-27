1696e2a811528732d14b35a163d64752e9e7849e
WojciechXi <wojciech.ratuszynski@gmail.com>
2025-10-27 23:13:17 +0100

    Update

--- Application/Resources/Material/Scripts/material.TextField.js
+++ Application/Resources/Material/Scripts/material.TextField.js
@@ -11,7 +11,15 @@ class TextFieldMaterial extends Layout {
         new Property(object, 'Icon', data.icon ?? null, object.OnPropertyChanged);
         new Property(object, 'Placeholder', data.placeholder ?? '', object.OnPropertyChanged);
         new Property(object, 'Value', data.value ?? '', object.OnPropertyChanged);
+
+        if (data.onInput) object.OnInput.Listen(data.onInput);
+        if (data.onChange) object.OnChange.Listen(data.onChange);
+        if (data.onPaste) object.OnPaste.Listen(data.onPaste);
     }
+
+    get OnInput() { return this.onInput ?? (this.onInput = new Callback()); }
+    get OnChange() { return this.onChange ?? (this.onChange = new Callback()); }
+    get OnPaste() { return this.onPaste ?? (this.onPaste = new Callback()); }
 
     Render() {
         let object = this;
@@ -25,16 +33,20 @@ class TextFieldMaterial extends Layout {
                 },
             }),
             new Row({
+                flexGrow: 1,
                 children: [
                     new Input({
                         onInput: function (sender, event) {
                             object.Value = sender.Value;
+                            object.OnInput.Invoke(object, event);
                         },
                         onChange: function (sender, event) {
                             object.Value = sender.Value;
+                            object.OnChange.Invoke(object, event);
                         },
                         onPaste: function (sender, event) {
                             object.Value = sender.Value;
+                            object.OnPaste.Invoke(object, event);
                         },
                         callback: function (view) {
                             new Binding(object, 'Value', function (sender, data) {
@@ -53,6 +65,13 @@ class TextFieldMaterial extends Layout {
                         text: 'close',
                         onClick: function (sender, event) {
                             object.Value = '';
+                            object.OnInput.Invoke(object, event);
+                            object.OnChange.Invoke(object, event);
+                        },
+                        callback: function (view) {
+                            new Binding(object, 'Value', function (sender, data) {
+                                view.Display = object.Value ? null : 'none';
+                            });
                         },
                     }),
                 ],
