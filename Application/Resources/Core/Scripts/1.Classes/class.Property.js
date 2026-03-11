4dd140188307717543f6b722fd00688b17c3a411
WojciechXi <wojciech.ratuszynski@gmail.com>
2026-03-12 00:45:44 +0100

    Update

--- Application/Resources/Core/Scripts/1.Classes/class.Property.js
+++ Application/Resources/Core/Scripts/1.Classes/class.Property.js
@@ -1,14 +1,15 @@
-class Property {
+class Property extends Disposable {
 
-    constructor(target, property, value = null, onChange = null, parser = null) {
+    constructor(target, property, value = null, onChange = null, parser = null, disposable = false) {
         let object = this;
 
         object.value = null;
 
         object.target = target;
         object.property = property;
-        object.parser = parser;
         object.onChange = onChange;
+        object.parser = parser;
+        object.disposable = disposable;
 
         Object.defineProperty(object.target, object.property, {
             get: function () {
@@ -19,6 +20,8 @@ class Property {
 
                 let oldValue = object.value;
                 if (oldValue != newValue) {
+                    if (object.disposable && oldValue instanceof Disposable) oldValue.Dispose();
+
                     object.value = newValue;
                     if (object.onChange) object.onChange.call(object.target, object.property, oldValue, newValue);
                 }
